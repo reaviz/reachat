fb41ebd1cee9a9141ab1f6608182fe597fd1d4d3
amcdnl <amcdaniel2@gmail.com>
2024-08-07 05:52:58 -0400

    add more story

--- stories/Chat.stories.tsx
+++ stories/Chat.stories.tsx
@@ -25,7 +25,7 @@ export default {
   component: Chat
 } as Meta;
 
-export const Basic = () => {
+export const Compact = () => {
   const [activeId, setActiveId] = useState<string>(fakeSessions[0].id);
   const [sessions, setSessions] = useState<Session[]>([
     ...fakeSessions,
@@ -40,6 +40,68 @@ export const Basic = () => {
         width: 350,
         height: 500,
         padding: 20,
+        borderRadius: 5
+      }}
+    >
+      <Chat
+        viewType="chat"
+        sessions={sessions}
+        activeSessionId={activeId}
+        onNewSession={() => {
+          const newId = (sessions.length + 1).toLocaleString();
+          setSessions([
+            ...sessions,
+            {
+              id: newId,
+              title: `New Session #${newId}`,
+              createdAt: new Date(),
+              updatedAt: new Date(),
+              conversations: []
+            }
+          ]);
+          setActiveId(newId);
+        }}
+        onSelectSession={setActiveId}
+        onDeleteSession={() => alert('delete!')}
+      >
+        <SessionMessagePanel>
+          <SessionMessages>
+            {conversations =>
+              conversations.map((conversation, index) => (
+                <SessionMessage
+                  key={conversation.id}
+                  conversation={conversation}
+                  isLast={index === conversations.length - 1}
+                />
+              ))
+            }
+          </SessionMessages>
+          <ChatInput />
+        </SessionMessagePanel>
+      </Chat>
+    </div>
+  );
+};
+
+export const FullScreen = () => {
+  const [activeId, setActiveId] = useState<string>(fakeSessions[0].id);
+  const [sessions, setSessions] = useState<Session[]>([
+    ...fakeSessions,
+    ...sessionsWithFiles,
+    ...sessionWithSources
+  ]);
+
+  return (
+    <div
+      className="dark:bg-gray-950 bg-white"
+      style={{
+        position: 'absolute',
+        top: 0,
+        left: 0,
+        right: 0,
+        bottom: 0,
+        padding: 20,
+        margin: 20,
         borderRadius: 5
       }}
     >
