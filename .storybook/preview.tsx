103f20897dc6930446c3a907231d78979b9e31b7
ivanzinchenko <zinchenkoivan92@gmail.com>
2025-03-24 12:23:07 +0200

    GOOD-541 change reablocks version

--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -16,7 +16,7 @@ const preview: Preview = {
         light: 'theme-light',
         dark: 'theme-dark',
       },
-      defaultTheme: 'theme-dark',
+      defaultTheme: 'dark',
     }),
   ],
   parameters: {
