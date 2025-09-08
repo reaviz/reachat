f7d217b435df5e844c2a70098d399be444d15955
Serhii.Tsybulskyi <c4r0n0s@gmail.com>
2025-09-08 12:36:24 +0300

    remove unused file

--- src/ChatInput/index.ts
+++ src/ChatInput/index.ts
@@ -2,5 +2,4 @@ export * from './ChatInput';
 export * from './FileInput';
 export * from './types';
 export * from './CommandDropdown';
-export * from './CommandItem';
 export * from './CommandIndicator';
