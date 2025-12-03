970e915e3948711033714b5e7568efb53d1e6243
jessie <jessiethanh.02@gmail.com>
2025-12-03 15:25:06 -0700

    Map through currentResponses to show button options

--- src/ChatPreBuiltResponses/ChatPreBuiltResponses.tsx
+++ src/ChatPreBuiltResponses/ChatPreBuiltResponses.tsx
@@ -8,10 +8,9 @@ interface ChatPreBuiltResponsesProps {
 
 export const ChatPreBuiltResponses: FC<ChatPreBuiltResponsesProps> = ({
   initialResponses,
-  followUpResponses
+  followUpResponses,
+  onSelectResponse /*This will be the parent's handler */
 }) => {
-  // pre-built will need to be a prop in the Chat component as optional
-
   //1. Define the pre built responses for intial and follow up (decided to have this be in the stories version)
   // Will move these to a separate file later
 
@@ -25,6 +24,11 @@ export const ChatPreBuiltResponses: FC<ChatPreBuiltResponsesProps> = ({
 
   //4. Change response options to buttons and an event handler to toggle the response between initial and followUp
 
+  const handleSelectResponse = (response: string) => {
+    onSelectResponse?.(response);
+    setResponse(response === 'initial' ? 'followUp' : 'initial');
+  };
+
   return (
     <div>
       <div className="h-30 rounded-md p-4 bg-gradient-to-bl from-transparent from-40% via-blue-500 via-70% to-indigo-500 to-80%">
@@ -32,17 +36,21 @@ export const ChatPreBuiltResponses: FC<ChatPreBuiltResponsesProps> = ({
         {/* Will refactor later to use theme colors and need to do dark and light mode*/}
         {/* Will need a compact size and a full size look. flex col to flex row*/}
         <p className="text-white text-lg font-bold">
-          Hi, What can I help you with?
+          {response === 'initial'
+            ? 'Hi, What can I help you with?'
+            : 'What else can I help you with?'}
         </p>
       </div>
       <div className="mt-4 flex flex-col gap-2">
-        <div className="dark:border-gray-700/50 dark:text-gray-200 dark:bg-gray-950 dark:hover:bg-blue-950/40 hover:bg-blue-100 hover:shadow-sm shadow-blue-500 hover:border-blue-500 border border-gray-200 h-12 rounded-md flex items-center justify-center">
-          option one
-        </div>{' '}
-        {/* will add in a click handler later and change to button */}
-        <div className="dark:border-gray-700/50 dark:text-gray-200 dark:bg-gray-950 dark:hover:bg-blue-950/40 hover:bg-blue-100 hover:shadow-sm shadow-blue-500 hover:border-blue-500 border border-gray-200 h-12 rounded-md flex items-center justify-center">
-          option two
-        </div>
+        {currentResponses?.map(response => (
+          <button
+            key={response.id}
+            className="dark:border-gray-700/50 dark:text-gray-200 dark:bg-gray-950 dark:hover:bg-blue-950/40 hover:bg-blue-100 hover:shadow-sm shadow-blue-500 hover:border-blue-500 border border-gray-200 h-12 rounded-md flex items-center justify-center"
+            onClick={() => handleSelectResponse(response.response)}
+          >
+            {response.response}
+          </button>
+        ))}
       </div>
     </div>
   );
