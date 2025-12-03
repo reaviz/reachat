bd54cb81fc85dac25b4627ccc14bd8d513bcbf0e
jessie <jessiethanh.02@gmail.com>
2025-12-02 18:19:52 -0700

    Match styling to input theme colors

--- src/ChatPreBuiltResponses/ChatPreBuiltResponses.tsx
+++ src/ChatPreBuiltResponses/ChatPreBuiltResponses.tsx
@@ -30,15 +30,19 @@ export const ChatPreBuiltResponses = () => {
 
   return (
     <div>
-      <div className="bg-pink-500 h-30 rounded-md">
-        Hi, What can I help you with?
+      <div className="border-gray-500 h-30 rounded-md p-4 bg-gradient-to-bl from-transparent from-30% via-blue-950 via-60% to-indigo-500 to-85">
+        {/* will add option for image later*/}
+        {/* Will refactor later to use theme colors*/}
+        <p className="text-white text-lg font-bold">
+          Hi, What can I help you with?
+        </p>
       </div>
-      <div className="mt-4 flex flex-col gap-4 bg-purple-400">
-        <div className="bg-blue-400 h-12 rounded-md border-2 flex items-center justify-center">
+      <div className="mt-4 flex flex-col gap-2">
+        <div className="dark:border-gray-700/50 dark:text-gray-200 dark:bg-gray-950 dark:hover:bg-blue-950/40 hover:bg-blue-100 hover:border-blue-500 border border-gray-200 h-12 rounded-md flex items-center justify-center">
           option one
         </div>{' '}
         {/* will add in a click handler later and change to button */}
-        <div className="bg-blue-400 h-12 rounded-md border-2 flex items-center justify-center">
+        <div className="dark:border-gray-700/50 dark:text-gray-200 dark:bg-gray-950 dark:hover:bg-blue-950/40 hover:bg-blue-100 hover:border-blue-500 border border-gray-200 h-12 rounded-md flex items-center justify-center">
           option two
         </div>
       </div>
