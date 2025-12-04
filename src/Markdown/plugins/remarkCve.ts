329971473ee464be50773eeb2b037820acb33142
amcdnl <amcdaniel2@gmail.com>
2025-12-04 15:52:53 -0500

    fix type

--- src/Markdown/plugins/remarkCve.ts
+++ src/Markdown/plugins/remarkCve.ts
@@ -1,20 +1,21 @@
 import type { Link, Root, Text } from 'mdast';
 import { findAndReplace } from 'mdast-util-find-and-replace';
+import type { Plugin } from 'unified';
 
 const CVE_REGEX = /(CVE-(19|20)\d{2}-\d{4,7})/gi;
 
-export function remarkCve() {
+export const remarkCve: Plugin<[], Root> = () => {
   return (tree: Root) => {
     findAndReplace(tree, [[CVE_REGEX, replaceCve]], {
       ignore: ['link', 'linkReference']
     });
   };
+};
 
-  function replaceCve(value: string): Link {
-    return {
-      type: 'link',
-      url: `https://cve.mitre.org/cgi-bin/cvename.cgi?name=${value}`,
-      children: [{ type: 'text', value: value.trim() } as Text]
-    };
-  }
+function replaceCve(value: string): Link {
+  return {
+    type: 'link',
+    url: `https://cve.mitre.org/cgi-bin/cvename.cgi?name=${value}`,
+    children: [{ type: 'text', value: value.trim() } as Text]
+  };
 }
