59072b24a6e3505e877d25eae92a7002b5540db6
jessie <jessiethanh.02@gmail.com>
2025-12-08 12:48:25 -0700

    Create options for independent component and pass as props

--- stories/ChatPreBuiltResponses.stories.tsx
+++ stories/ChatPreBuiltResponses.stories.tsx
@@ -1,11 +1,10 @@
 import { Meta, StoryObj } from '@storybook/react';
-import { Chat, ChatInput, Session, SessionMessages, SessionMessagePanel } from '../src';
+import { Chat, ChatInput, SessionMessagePanel } from '../src';
 import {
     fakeSessions,
   } from './examples';
 import { useState, FC } from 'react';
 import { OpenAI } from 'openai';
-import { Input } from 'reablocks';
 import { ChatPreBuiltResponses } from '../src'
 
 export default {
@@ -13,54 +12,13 @@ export default {
     component: ChatPreBuiltResponses
   } as Meta<typeof ChatPreBuiltResponses>;
 
-  type Story = StoryObj<typeof ChatPreBuiltResponses>;
-
-const endConversationOption = {
-  id: 'all_done',
-  response: "That's all I need, thanks!",
-  followUpHeaderText: "Thanks for stopping by!",
-  followUps: [
-    { id: 'changed_mind', response: "Actually, I have another question." },
-    { id: 'start_over', response: "Start over" }
-  ]
-};
-
-const preBuiltResponses = [
-  {
-    id: 'browsing',
-    response: "I'm just browsing, thanks for asking.",
-    followUpHeaderText: "Thanks for stopping by!",
-    followUps: [
-      { id: 'changed_mind', response: "Actually, I changed my mind - I have a question." },
-      { id: 'start_over', response: "Start over" }
-    ]
-  },
-  {
-    id: 'support',
-    response: 'I need support with my account.',
-    followUpHeaderText: "What kind of support do you need?",
-    followUps: [
-      { id: 'billing', response: "I have a billing question." },
-      { id: 'technical', response: "I'm having a technical issue." },
-      { id: 'password', response: "I need to reset my password." },
-      { id: 'representative', response: "I'd like to speak to someone." },
-      endConversationOption
-    ]
-  },
-  {
-    id: 'pricing',
-    response: 'Tell me about your pricing.',
-    followUpHeaderText: "What would you like to know about pricing?",
-    followUps: [
-      { id: 'compare', response: "How does this compare to competitors?" },
-      { id: 'trial', response: "Is there a free trial?" },
-      { id: 'enterprise', response: "Do you have enterprise pricing?" },
-      endConversationOption
-    ]
-  }
-];
-
   export const IndependentComponent: FC = () => {
+
+    const initialOptions = [
+      { id: '1', response: "I'm just browsing, thanks for asking." },
+      { id: '2', response: 'I need support with my account.' },
+      { id: '3', response: 'Tell me about your pricing.' },
+    ];
     return (
         <div className="dark:bg-gray-950 bg-white"
         style={{
@@ -68,7 +26,7 @@ const preBuiltResponses = [
           height: 500,
           padding: 40
         }}>
-           <ChatPreBuiltResponses responses={preBuiltResponses} />
+           <ChatPreBuiltResponses options={initialOptions} />
         </div>
     );
   }
